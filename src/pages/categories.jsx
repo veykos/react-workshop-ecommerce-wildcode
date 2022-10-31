41c8b95edc3d5c44244f9aee939275d0973a1641
Pavel Popov <pcveykos@gmail.com>
2022-10-31 13:39:34 +0100

    Step 4: Store axios api call data in state, take care of any errors

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -3,46 +3,29 @@ import { CategoryItem } from "../components/CategoryItem/CategoryItem";
 import { useEffect, useState } from "react";
 import axios from 'axios'
 export default function Categories() {
-  const data = [
-    {
-      id: 1,
-      name: "Clothes",
-      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=6011",
-    },
-    {
-      id: 2,
-      name: "Electronics",
-      image: "https://api.lorem.space/image/watch?w=640&h=480&r=304",
-    },
-    {
-      id: 3,
-      name: "Furniture",
-      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=9450",
-    },
-    {
-      id: 4,
-      name: "Shoes",
-      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=3133",
-    },
-    {
-      id: 5,
-      name: "Others",
-      image: "https://api.lorem.space/image?w=640&h=480&r=4432",
-    },
-  ];
+  const [categoriesResp, setCategoriesResp] = useState([]);
+  const [errorResp, setErrorResp] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
     axios.get('https://api.escuelajs.co/api/v1/categories')
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
+    .then(response => setCategoriesResp(response))
+    .catch(error => setErrorResp(error, "error"))
+    .finally(() => setIsLoading(false));
 
   }, [])
   
-  
+  if (isLoading) {
+    return <h2>request is still in process, loading..</h2>;
+  }
+  if (errorResp) {
+    console.log(errorResp, 'error')
+    return <h2>an error has occurred, please contact the support</h2>;
 
-
+  }
   return (
     <div>
-    <CategoriesList data={data}/>
+    <CategoriesList data={categoriesResp.data}/>
     <CategoryItem/>
   </div>
 );
