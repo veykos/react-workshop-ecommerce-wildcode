6d63fbc3f7e2508b59fb34e2f286761aa6de9623
Pavel Popov <pcveykos@gmail.com>
2022-10-31 13:32:58 +0100

    Step 3: Refactor component and add axios api call

--- src/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesList/CategoriesList.jsx
@@ -1,34 +1,7 @@
 import styles from './styles.module.css'
 
-export const CategoriesList = () => {
-  const data = [
-    {
-      id: 1,
-      name: "Clothes",
-      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=6011",
-    },
-    {
-      id: 2,
-      name: "Electronics",
-      image: "https://api.lorem.space/image/watch?w=640&h=480&r=304",
-    },
-    {
-      id: 3,
-      name: "Furniture",
-      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=9450",
-    },
-    {
-      id: 4,
-      name: "Shoes",
-      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=3133",
-    },
-    {
-      id: 5,
-      name: "Others",
-      image: "https://api.lorem.space/image?w=640&h=480&r=4432",
-    },
-  ];
-
+export const CategoriesList = (props) => {
+  const {data} = props
 
   return(
       <div className={styles.layout} >
--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,9 +1,48 @@
 import { CategoriesList } from "../components/CategoriesList/CategoriesList";
 import { CategoryItem } from "../components/CategoryItem/CategoryItem";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 export default function Categories() {
+  const data = [
+    {
+      id: 1,
+      name: "Clothes",
+      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=6011",
+    },
+    {
+      id: 2,
+      name: "Electronics",
+      image: "https://api.lorem.space/image/watch?w=640&h=480&r=304",
+    },
+    {
+      id: 3,
+      name: "Furniture",
+      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=9450",
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=3133",
+    },
+    {
+      id: 5,
+      name: "Others",
+      image: "https://api.lorem.space/image?w=640&h=480&r=4432",
+    },
+  ];
+  useEffect(() => {
+    axios.get('https://api.escuelajs.co/api/v1/categories')
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+  }, [])
+  
+  
+
+
   return (
     <div>
-    <CategoriesList/>
+    <CategoriesList data={data}/>
     <CategoryItem/>
   </div>
 );
